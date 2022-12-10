5282643d5df9a6131279852ac45bcb5ee08774cb
jessicalopesvs <37242604+jessicalopesvs@users.noreply.github.com>
2022-12-10 23:19:41 +0000

    Admin functions

--- src/main/java/com/nci/webapp/AlzApp/controller/AdminController.java
+++ src/main/java/com/nci/webapp/AlzApp/controller/AdminController.java
@@ -1,26 +1,40 @@
 package com.nci.webapp.AlzApp.controller;
 
+import com.nci.webapp.AlzApp.model.Report;
+import com.nci.webapp.AlzApp.model.User;
 import com.nci.webapp.AlzApp.repository.ReportRepository;
+import com.nci.webapp.AlzApp.repository.UserRepository;
 import com.nci.webapp.AlzApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private ReportRepository reportRepository;
 
 
+    @GetMapping("/users")
+    public String users(Model model){
+        List<User> users = userRepository.findAll();
+        List<Report> reports = reportRepository.findAll();
+        int reportQtt = reports.size();
 
-    @GetMapping("/user-list")
-    public String editUser(){
-
-    return "admin/user-list";
+        model.addAttribute("quantityReports", reportQtt);
+        model.addAttribute("users", users);
+        return "user/users";
     }
 }
