0ba949f5e0649359f99facb393449715fe0d64e5
jessicalopesvs <37242604+jessicalopesvs@users.noreply.github.com>
2022-12-11 14:48:16 +0000

    home

--- src/main/java/com/nci/webapp/AlzApp/controller/HomeController.java
+++ src/main/java/com/nci/webapp/AlzApp/controller/HomeController.java
@@ -1,16 +1,21 @@
 package com.nci.webapp.AlzApp.controller;
 
 
+import com.nci.webapp.AlzApp.model.Symptoms;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String index() {
+
         return "home";
     }
 
